0b763eeba063449c8d73fcfac9467902864718f5
dev-krishna78 <krishnacarpenter222@gmail.com>
2025-04-11 10:25:27 +0530

    modified after cloning

--- launch.java
+++ launch.java
@@ -1,5 +1,14 @@
 class launch{
    public static void main(String[]args){
     System.out.print(" understanding the git and git hub");
+    System.out.print(" this is my first git clone and puch operation");
+
+}
+   public static void sum(){
+   
+  int a = 10;
+int b =20; 
+int sum = a+b;
+System.out.println(sum);
 }
 }
